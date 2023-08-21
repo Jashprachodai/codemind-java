c63a222b6383bd5592dfcc8b85d63e0ab2ce3fcf
JashPrachodai <119839259+Jashprachodai@users.noreply.github.com>
2023-08-21 11:26:24 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] ar){         Scanner ob = new Scanner(System.in);         int a,b;         a=ob.nextInt();         b=ob.nextInt();         int arr[][] = new int[a][b];         int s=0;         for(int i=0;i<a;i++){             for(int j=0;j<b;j++){                 arr[i][j]=ob.nextInt();                 s+=arr[i][j];             }             System.out.print(s + " ");             s=0;         }
        }
    }

---
+++ Row_wise_sum_of_a_matrix.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] ar){
+        Scanner ob = new Scanner(System.in);
+        int a,b;
+        a=ob.nextInt();
+        b=ob.nextInt();
+        int arr[][] = new int[a][b];
+        int s=0;
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                arr[i][j]=ob.nextInt();
+                s+=arr[i][j];
+            }
+            System.out.print(s + " ");
+            s=0;
+        }
+        
+    }
+}
