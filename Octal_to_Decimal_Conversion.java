72e74ab1d620c53033f4f0feee21af8b848b97fa
JashPrachodai <119839259+Jashprachodai@users.noreply.github.com>
2023-08-25 10:02:01 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] ar){         Scanner ob = new Scanner(System.in);         String n = ob.next();         System.out.print(Integer.parseInt(n,8));     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] ar){
+        Scanner ob = new Scanner(System.in);
+        String n = ob.next();
+        System.out.print(Integer.parseInt(n,8));
+    }
+}
