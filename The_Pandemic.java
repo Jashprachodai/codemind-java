a3b89c6472bfbdb4c60a9d6a2fbe9678056e87eb
JashPrachodai <119839259+Jashprachodai@users.noreply.github.com>
2023-08-25 10:27:47 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] ar){         Scanner ob = new Scanner(System.in);         int t = ob.nextInt();         if(t==1)         System.out.print(0);         else if(t==2)         System.out.print(1);         else         System.out.print(2);
        }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] ar){
+        Scanner ob = new Scanner(System.in);
+        int t = ob.nextInt();
+        if(t==1)
+        System.out.print(0);
+        else if(t==2)
+        System.out.print(1);
+        else
+        System.out.print(2);
+        
+    }
+}
