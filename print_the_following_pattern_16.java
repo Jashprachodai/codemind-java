26d1861f20ed3063f2db6e137b631938aec4a024
JashPrachodai <119839259+Jashprachodai@users.noreply.github.com>
2023-09-07 20:11:42 +0530

    import java.util.*; public class Main{     public static void main(String[] aerfadx){         Scanner ob = new Scanner(System.in);         int n = ob.nextInt();         for(int i=1;i<=n;i++){             for(int j=1;j<=n;j++){                 System.out.print(i + " ");             }             System.out.println();         }     } }

---
+++ print_the_following_pattern_16.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] aerfadx){
+        Scanner ob = new Scanner(System.in);
+        int n = ob.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
