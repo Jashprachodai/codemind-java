cfa3b561c1af8878cf4e82f5babc81f56b41f2c7
JashPrachodai <119839259+Jashprachodai@users.noreply.github.com>
2023-09-07 19:43:53 +0530

    import java.util.*; public class Main{     public static void main(String[] aerfadx){         Scanner ob = new Scanner(System.in);         int n = ob.nextInt();         while(true){             n+=1;             if(pali(n) && prime(n))             break;         }         System.out.print(n);     }     public static boolean pali(int n){         int t=n,x=0;         while(n!=0){             x=x*10 + (n%10);             n=n/10;         }         return t==x;     }     public static boolean prime(int n){         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         if(n%i==0)         return false;         return true;     } }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main{
-    public static void main(String[] arg){
+    public static void main(String[] aerfadx){
         Scanner ob = new Scanner(System.in);
         int n = ob.nextInt();
         while(true){
@@ -10,22 +10,20 @@ public class Main{
         }
         System.out.print(n);
     }
+    public static boolean pali(int n){
+        int t=n,x=0;
+        while(n!=0){
+            x=x*10 + (n%10);
+            n=n/10;
+        }
+        return t==x;
+    }
     public static boolean prime(int n){
         if(n==1)
         return false;
-        for(int i=2;i<=(int)Math.sqrt(n);i++){
-            if(n%i==0)
-            return false;
-        }
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        if(n%i==0)
+        return false;
         return true;
-    }
-    public static boolean pali(int n){
-        int s=0,x,t=n;
-        while(n!=0){
-            x=n%10;
-            s=s*10+x;
-            n=n/10;
-        }
-        return t==s;
     }
 }
